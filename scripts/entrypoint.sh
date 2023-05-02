7b0d1b0b5986be376c4a024946c081c15f972325
8baller <tomrae030@hotmail.com>
2023-05-02 09:04:56 +0100

    [feat] entrypoint script

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+# params
+# $1: subgraph config (mainnet or goerli)
+
+set -e
+
+echo "Starting entrypoint.sh"
+echo "Network: $CHAIN"
+echo "Subgraph node: $SUBGRAPH_NODE"
+echo "Registry: $IPFS_REGISTRY"
+
+
+if [ "$CHAIN" = "mainnet" ]; then
+  echo "Starting mainnet"
+  cp profiles/subgraph-prod.yaml subgraph.yaml
+elif [ "$CHAIN" = "goerli" ]; then
+  echo "Starting "goerli"
+  cp profiles/subgraph-staging.yaml subgraph.yaml
+else
+  echo "Invalid param"
+fi
+
+yarn graph codegen &&  yarn graph build
+
+yarn graph remove --node $SUBGRAPH_NODE autonolas || echo "Not Found!" 
+yarn graph create --node $SUBGRAPH_NODE autonolas
+yarn graph deploy --node $SUBGRAPH_NODE --ipfs $IPFS_REGISTRY autonolas -l 0.1.0
