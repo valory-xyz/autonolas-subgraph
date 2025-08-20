1263c135c7c2bb450562e83aaaffe506f6eeafa8
Atatakai <densatsu@mail.ru>
2025-08-20 17:25:16 +0200

    fix: increase pruning for mech

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,33 +5,33 @@ echo "Subgraph node: $SUBGRAPH_NODE"
 echo "Registry: $IPFS_REGISTRY"
 
 # Install the "autonolas" Subgraph
-cd /subgraphs/autonolas
+# cd /subgraphs/autonolas
 
-yarn graph codegen && yarn graph build
+# yarn graph codegen && yarn graph build
 
-until yarn graph create --node $SUBGRAPH_NODE autonolas
-do
-  echo "graph-node not ready..."
-  sleep 10
-done
-yarn graph deploy --node $SUBGRAPH_NODE --ipfs $IPFS_REGISTRY autonolas -l 0.1.0
+# until yarn graph create --node $SUBGRAPH_NODE autonolas
+# do
+#   echo "graph-node not ready..."
+#   sleep 10
+# done
+# yarn graph deploy --node $SUBGRAPH_NODE --ipfs $IPFS_REGISTRY autonolas -l 0.1.0
 
-# Install the "autonolas-staging" Subgraph
-cd /subgraphs/autonolas
-# We are deploying autonolas-staging for backward compatibility (there are some applications using it)
-# It has a different startBlock to be indexed separately from “autonolas” Subgraph
-sed -i 's/startBlock: 15178253/startBlock: 15178252/g' subgraph.yaml
+# # Install the "autonolas-staging" Subgraph
+# cd /subgraphs/autonolas
+# # We are deploying autonolas-staging for backward compatibility (there are some applications using it)
+# # It has a different startBlock to be indexed separately from “autonolas” Subgraph
+# sed -i 's/startBlock: 15178253/startBlock: 15178252/g' subgraph.yaml
 
-yarn graph codegen &&  yarn graph build
-yarn graph create --node $SUBGRAPH_NODE autonolas-staging
-yarn graph deploy --node $SUBGRAPH_NODE --ipfs $IPFS_REGISTRY autonolas-staging -l 0.1.0
+# yarn graph codegen &&  yarn graph build
+# yarn graph create --node $SUBGRAPH_NODE autonolas-staging
+# yarn graph deploy --node $SUBGRAPH_NODE --ipfs $IPFS_REGISTRY autonolas-staging -l 0.1.0
 
-# Install the "autonolas-base" Subgraph
-cd /subgraphs/autonolas-base
+# # Install the "autonolas-base" Subgraph
+# cd /subgraphs/autonolas-base
 
-yarn graph codegen &&  yarn graph build
-yarn graph create --node $SUBGRAPH_NODE autonolas-base
-yarn graph deploy --node $SUBGRAPH_NODE --ipfs $IPFS_REGISTRY autonolas-base -l 0.1.0
+# yarn graph codegen &&  yarn graph build
+# yarn graph create --node $SUBGRAPH_NODE autonolas-base
+# yarn graph deploy --node $SUBGRAPH_NODE --ipfs $IPFS_REGISTRY autonolas-base -l 0.1.0
 
 
 # Install the "mech" Subgraph
