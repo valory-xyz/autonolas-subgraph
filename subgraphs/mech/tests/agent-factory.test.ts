93f788bfbbef28da20cac304aa6ac12f2c47ee23
Skanislav <skas.merkushin@gmail.com>
2025-10-20 16:51:42 +0200

    chore: fix existing tests

--- subgraphs/mech/tests/agent-factory.test.ts
+++ subgraphs/mech/tests/agent-factory.test.ts
@@ -16,14 +16,6 @@ import { createCreateMechEvent } from "./agent-factory-utils"
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
 
 describe("Describe entity assertions", () => {
-  beforeAll(() => {
-    let mech = Address.fromString("0x0000000000000000000000000000000000000001")
-    let agentId = BigInt.fromI32(234)
-    let price = BigInt.fromI32(234)
-    let newCreateMechEvent = createCreateMechEvent(mech, agentId, price)
-    handleCreateMech(newCreateMechEvent)
-  })
-
   afterAll(() => {
     clearStore()
   })
@@ -32,24 +24,30 @@ describe("Describe entity assertions", () => {
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
   test("CreateMech created and stored", () => {
+    let mech = Address.fromString("0x0000000000000000000000000000000000000001")
+    let agentId = BigInt.fromI32(234)
+    let price = BigInt.fromI32(234)
+    let newCreateMechEvent = createCreateMechEvent(mech, agentId, price)
+    handleCreateMech(newCreateMechEvent)
+
     assert.entityCount("CreateMech", 1)
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
       "CreateMech",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      mech.toHexString(),
       "mech",
       "0x0000000000000000000000000000000000000001"
     )
     assert.fieldEquals(
       "CreateMech",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      mech.toHexString(),
       "agentId",
       "234"
     )
     assert.fieldEquals(
       "CreateMech",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      mech.toHexString(),
       "price",
       "234"
     )
