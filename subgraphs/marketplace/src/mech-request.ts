aaea1b1cbcc583fa537c22f1794709e742318cf7
Skanislav <skas.merkushin@gmail.com>
2025-11-04 11:55:12 +0100

    feat: improve request parsing

--- subgraphs/marketplace/src/mech-request.ts
+++ subgraphs/marketplace/src/mech-request.ts
@@ -1,8 +1,10 @@
-import { Bytes, dataSource, json, log } from "@graphprotocol/graph-ts";
+import { Bytes, dataSource, json, JSONValueKind, log } from "@graphprotocol/graph-ts";
 import { ParsedRequest } from "../generated/schema";
 
+let UNHANDLED_TYPE = '[unhandled type]';
+
 export function handleMechRequest(content: Bytes): void {
-    // dataSource.stringParams() returns the File DataSource CID
+  // dataSource.stringParams() returns the File DataSource CID
   // stringParam() will be mocked in the handler test
   // for more info https://thegraph.com/docs/en/developing/creating-a-subgraph/#create-a-new-handler-to-process-files
 
@@ -10,6 +12,25 @@ export function handleMechRequest(content: Bytes): void {
   let context = dataSource.context();
   let requestId = context.getBytes('requestId');
   let parsedRequest = new ParsedRequest(requestId);
+
+  let obj = json.fromBytes(content)
+
+  // Empty by default
+  parsedRequest.prompt = UNHANDLED_TYPE;
+  parsedRequest.tool = UNHANDLED_TYPE;
+
+  if (obj.kind === JSONValueKind.OBJECT) {
+    let parsed = obj.toObject();
+    let prompt = parsed.get('prompt');
+    let tool = parsed.get('tool');
+    if (prompt !== null && prompt.kind === JSONValueKind.STRING) {
+      log.info("Succesfully parsed prompt: {}", [prompt.toString()]);
+      parsedRequest.prompt = prompt.toString();
+    }
+    if (tool !== null && tool.kind === JSONValueKind.STRING) {
+      parsedRequest.tool = tool.toString();
+    }
+  }
 
   parsedRequest.request = requestId;
   parsedRequest.content = content.toString();
--- subgraphs/marketplace/tests/mech-requests.test.ts
+++ subgraphs/marketplace/tests/mech-requests.test.ts
@@ -46,5 +46,31 @@ describe("Describe mech requests processing", () => {
         assert.fieldEquals("ParsedRequest", requestId.toHexString(), "hash", cid);
         assert.fieldEquals("ParsedRequest", requestId.toHexString(), "request", requestId.toHexString());
 
+        assert.fieldEquals("ParsedRequest", requestId.toHexString(), "prompt", "With the given question \"Will the average price of a gallon of gas in the United States reach at least $3.30 by June 19, 2025, in response to the Israel-Iran conflict?\" and the `yes` option represented by `Yes` and the `no` option represented by `No`, what are the respective probabilities of `p_yes` and `p_no` occurring?");
+        assert.fieldEquals("ParsedRequest", requestId.toHexString(), "tool", "prediction-request-rag");
+    })
+
+    test("Handle mech request invalid object", () => {
+        // arrange
+        const cid = "f01701220deadbeef/234";
+        let requestId = Bytes.fromHexString("0x1234567890abcdef");
+
+        let context = new DataSourceContext();
+        context.setBytes('requestId', requestId);
+
+        dataSourceMock.resetValues();
+        dataSourceMock.setAddressAndContext(cid, context);
+
+        let request = Bytes.fromHexString("0xdeadbeef");
+        log.info("request: {}", [request.toString()]);
+
+        handleMechRequest(request);
+
+        assert.entityCount("ParsedRequest", 1);
+        assert.fieldEquals("ParsedRequest", requestId.toHexString(), "content", request.toString());
+        assert.fieldEquals("ParsedRequest", requestId.toHexString(), "hash", cid);
+        assert.fieldEquals("ParsedRequest", requestId.toHexString(), "request", requestId.toHexString());
+        assert.fieldEquals("ParsedRequest", requestId.toHexString(), "prompt", "[unhandled type]");
+        assert.fieldEquals("ParsedRequest", requestId.toHexString(), "tool", "[unhandled type]");
     })
 })  
