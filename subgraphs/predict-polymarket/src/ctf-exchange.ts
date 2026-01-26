830fa6caf9782fc0df5162972a0fa81a6d1cc359
Atatakai <densatsu@mail.ru>
2026-01-26 17:21:03 +0100

    feat: add predict-polymarket

---
+++ subgraphs/predict-polymarket/src/ctf-exchange.ts
@@ -0,0 +1,100 @@
+import { BigInt, Bytes, log } from "@graphprotocol/graph-ts";
+import {
+  OrderFilled as OrderFilledEvent,
+  TokenRegistered as TokenRegisteredEvent,
+} from "../generated/CTFExchange/CTFExchange";
+import {
+  Bet,
+  Question,
+  TokenRegistry,
+  TraderAgent,
+} from "../generated/schema";
+import { getDailyProfitStatistic, processTradeActivity } from "./utils";
+
+export function handleTokenRegistered(event: TokenRegisteredEvent): void {
+  // Register Outcome 0 (Usually "No")
+  let token0Id = Bytes.fromByteArray(Bytes.fromBigInt(event.params.token0));
+  let registry0 = new TokenRegistry(token0Id);
+  registry0.tokenId = event.params.token0;
+  registry0.conditionId = event.params.conditionId;
+  registry0.outcomeIndex = BigInt.fromI32(0);
+  registry0.transactionHash = event.transaction.hash;
+  registry0.save();
+
+  // Register Outcome 1 (Usually "Yes")
+  let token1Id = Bytes.fromByteArray(Bytes.fromBigInt(event.params.token1));
+  let registry1 = new TokenRegistry(token1Id);
+  registry1.tokenId = event.params.token1;
+  registry1.conditionId = event.params.conditionId;
+  registry1.outcomeIndex = BigInt.fromI32(1);
+  registry1.transactionHash = event.transaction.hash;
+  registry1.save();
+}
+
+export function handleOrderFilled(event: OrderFilledEvent): void {
+  // 1. Identify if the maker is one of our TraderAgents
+  let agentId = event.params.maker;
+  let agent = TraderAgent.load(agentId);
+
+  if (agent === null) return;
+
+  // 2. Identify the Trade direction and quantities
+  // makerAssetId == 0 means Maker gave USDC and received Tokens (BUYING)
+  // takerAssetId == 0 means Taker gave USDC and Maker gave Tokens (SELLING)
+  let isBuying = event.params.makerAssetId.isZero();
+
+  // The amount of USDC (money) involved in the trade
+  let usdcAmount = isBuying ? event.params.makerAmountFilled : event.params.takerAmountFilled;
+
+  // The amount of Shares (tokens) involved in the trade
+  let sharesAmount = isBuying ? event.params.takerAmountFilled : event.params.makerAmountFilled;
+
+  // The token ID of the outcome being traded
+  let outcomeTokenId = isBuying ? event.params.takerAssetId : event.params.makerAssetId;
+
+  // 3. Lookup the outcome index from our Registry
+  let tokenRegistry = TokenRegistry.load(Bytes.fromByteArray(Bytes.fromBigInt(outcomeTokenId)));
+  if (tokenRegistry === null) {
+    log.warning("TokenRegistry missing for token {} in tx {}", [
+      outcomeTokenId.toString(),
+      event.transaction.hash.toHexString()
+    ]);
+    return;
+  }
+
+  // 4. Update Daily Stats
+  let dailyStat = getDailyProfitStatistic(agent.id, event.block.timestamp);
+  dailyStat.totalBets += 1;
+  dailyStat.totalTraded = dailyStat.totalTraded.plus(usdcAmount);
+  dailyStat.save();
+
+  // 5. Initialize Bet
+  let betId = event.transaction.hash.concat(Bytes.fromI32(event.logIndex.toI32()));
+  let bet = new Bet(betId);
+  bet.bettor = agent.id;
+  bet.outcomeIndex = tokenRegistry.outcomeIndex;
+  bet.amount = usdcAmount;
+  bet.shares = sharesAmount;
+  bet.blockTimestamp = event.block.timestamp;
+  bet.transactionHash = event.transaction.hash;
+  bet.dailyStatistic = dailyStat.id;
+  bet.countedInTotal = false;
+  bet.countedInProfit = false;
+
+  let question = Question.load(tokenRegistry.conditionId);
+  if (question !== null) {
+    bet.question = question.id;
+  }
+  bet.save();
+
+  // 6. Process Agent, Participant, and Global atomically
+  processTradeActivity(
+    agent,
+    tokenRegistry.conditionId,
+    betId,
+    usdcAmount,
+    event.block.timestamp,
+    event.block.number,
+    event.transaction.hash
+  );
+}
---
+++ subgraphs/predict-polymarket/src/utils.ts
@@ -0,0 +1,139 @@
+import { BigInt, Bytes, Address } from "@graphprotocol/graph-ts";
+import { Global, TraderAgent, MarketParticipant, DailyProfitStatistic } from "../generated/schema";
+import { ONE_DAY } from "./constants";
+
+/**
+ * Return global entity for updates
+ * Create new if doesn't exist
+ */
+export function getGlobal(): Global {
+  let global = Global.load("");
+  if (global == null) {
+    global = new Global("");
+    global.totalTraderAgents = 0;
+    global.totalActiveTraderAgents = 0;
+    global.totalBets = 0;
+    global.totalTraded = BigInt.zero();
+    global.totalTradedSettled = BigInt.zero();
+    global.totalPayout = BigInt.zero();
+  }
+  return global as Global;
+}
+
+/**
+ * Helper for saving entities in maps (batch save optimization)
+ */
+export function saveMapValues<T>(map: Map<string, T>): void {
+  let values = map.values();
+  // @ts-ignore - AssemblyScript Map.values() returns array-like structure
+  for (let i = 0; i < values.length; i++) {
+    // @ts-ignore - Graph-cli entities have a .save() method
+    values[i].save();
+  }
+}
+
+/**
+ * Get the timestamp for the start of the day (UTC midnight)
+ */
+export function getDayTimestamp(timestamp: BigInt): BigInt {
+  return timestamp.div(ONE_DAY).times(ONE_DAY);
+}
+
+/**
+ * Get or create daily profit statistic for an agent on a specific day
+ */
+export function getDailyProfitStatistic(
+  agentAddress: Bytes,
+  timestamp: BigInt
+): DailyProfitStatistic {
+  let dayTimestamp = getDayTimestamp(timestamp);
+  let id = agentAddress.toHexString() + "_" + dayTimestamp.toString();
+  let statistic = DailyProfitStatistic.load(id);
+
+  if (statistic == null) {
+    statistic = new DailyProfitStatistic(id);
+    statistic.traderAgent = agentAddress;
+    statistic.date = dayTimestamp;
+    statistic.totalBets = 0;
+    statistic.totalTraded = BigInt.zero();
+    statistic.totalPayout = BigInt.zero();
+    statistic.dailyProfit = BigInt.zero();
+    statistic.profitParticipants = [];
+  }
+  return statistic as DailyProfitStatistic;
+}
+
+/**
+ * add profit participant into profit statistic
+ * should be called when profit changes:
+ * - on market settlement if bets were incorrect
+ * - on payout if bets were correct
+ */
+export function addProfitParticipant(
+  statistic: DailyProfitStatistic,
+  questionId: Bytes
+): void {
+  let participants = statistic.profitParticipants;
+  if (participants.indexOf(questionId) == -1) {
+    participants.push(questionId);
+    statistic.profitParticipants = participants;
+  }
+}
+
+/**
+ * Consolidates all activity and volume updates into a single pass.
+ */
+export function processTradeActivity(
+  agent: TraderAgent,
+  conditionId: Bytes,
+  betId: Bytes,
+  amount: BigInt,
+  timestamp: BigInt,
+  blockNumber: BigInt,
+  txHash: Bytes
+): void {
+  let global = getGlobal();
+
+  // 1. Update Global
+  global.totalBets += 1;
+  global.totalTraded = global.totalTraded.plus(amount);
+
+  // 2. Update TraderAgent
+  if (agent.firstParticipation === null) {
+    agent.firstParticipation = timestamp;
+    global.totalActiveTraderAgents += 1;
+  }
+  agent.totalBets += 1;
+  agent.lastActive = timestamp;
+  agent.totalTraded = agent.totalTraded.plus(amount);
+
+  // 3. Update or Create MarketParticipant
+  let participantId = agent.id.toHexString() + "_" + conditionId.toHexString();
+  let participant = MarketParticipant.load(participantId);
+  
+  if (participant == null) {
+    participant = new MarketParticipant(participantId);
+    participant.traderAgent = agent.id;
+    participant.question = conditionId; // Polymarket uses 'question' as the field name
+    participant.totalBets = 0;
+    participant.totalTraded = BigInt.zero();
+    participant.totalTradedSettled = BigInt.zero();
+    participant.totalPayout = BigInt.zero();
+    participant.createdAt = timestamp;
+    participant.bets = [];
+  }
+
+  let bets = participant.bets;
+  bets.push(betId);
+  participant.bets = bets;
+  participant.totalBets += 1;
+  participant.totalTraded = participant.totalTraded.plus(amount);
+  participant.blockTimestamp = timestamp;
+  participant.blockNumber = blockNumber;
+  participant.transactionHash = txHash;
+
+  // 4. Save all
+  global.save();
+  agent.save();
+  participant.save();
+}
